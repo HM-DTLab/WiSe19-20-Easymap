4dee1d306184aad3d3e78ae891a309d14e8029d6
Ben <benjaminvanni97@gmail.com>
2019-12-18 14:00:55 +0100

    auth

--- stable/lib/login_site.dart
+++ stable/lib/login_site.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +25,38 @@ class LoginPage extends StatelessWidget {
           backgroundColor: Colors.green,
         ),
         body: Container(
-          alignment: Alignment.center,
-          child: RaisedButton(
-            child: Text("Here comes the Login page..."),
-            onPressed: () {},
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                Text(
+                  'Login Information',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    onSaved: (value) => _email = value,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: "Email Address")),
+                TextFormField(
+                    onSaved: (value) => _password = value,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password")),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                    child: Text("LOGIN"),
+                    onPressed: () {
+                      final form = _formKey.currentState;
+                      form.save();
+                      // Validate will return true if is valid, or false if invalid.
+                      if (form.validate()) {
+                        print("$_email $_password");
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
       ),
--- stable/lib/main.dart
+++ stable/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:stable/add_contact.dart';
+import 'auth_service.dart';
 import 'home.dart';
 import 'login_site.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      ChangeNotifierProvider<AuthService>(
+        child: MyApp(),
+        builder: (BuildContext context) {
+          return AuthService();
+        },
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,7 +20,19 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => Home(),
+        '/': (context) => FutureBuilder(
+              // get the Provider, and call the getUser method
+              future: Provider.of<AuthService>(context).getUser(),
+              // wait for the future to resolve and render the appropriate
+              // widget for HomePage or LoginPage
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return snapshot.hasData ? Home() : LoginPage();
+                } else {
+                  return Container(color: Colors.white);
+                }
+              },
+            ),
         // When navigating to the "/second" route, build the SecondScreen widget.
         '/addcontact': (context) => AddContact(),
         '/login': (context) => LoginPage(),
