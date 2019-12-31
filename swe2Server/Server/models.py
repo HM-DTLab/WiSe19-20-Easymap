47547c1a6bb39aa514c4337a9c11668cf85f7cea
Fabian0xE <feckardt@hm.edu>
2019-12-31 14:42:29 +0100

    Routes can now be added for the Users

--- swe2Server/Server/models.py
+++ swe2Server/Server/models.py
@@ -6,7 +6,17 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
+    routes = db.relationship('Routes', backref='author', lazy=True)
 
     def __repr__(self):
         return f'User {self.id}: ({self.username}, {self.email})'
 
+
+class Routes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    Address = db.Column(db.String(512), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'Route {self.id}: ({self.title}, {self.Address})'
